db2577851d17cc6ff6551cbe27c6355235349c40
usama <usamaiqb@yahoo.com>
2026-02-21 16:29:40 +0500

    search position fix

--- app/src/main/java/com/example/noty/ui/NotyApp.kt
+++ app/src/main/java/com/example/noty/ui/NotyApp.kt
@@ -15,6 +15,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -185,6 +186,7 @@ fun NotyApp(
                     searchActive = it
                     if (!it) searchQuery = ""
                 },
+                windowInsets = WindowInsets(0),
                 modifier = Modifier
                     .fillMaxWidth()
                     .padding(horizontal = searchBarHPadding, vertical = searchBarVPadding),
